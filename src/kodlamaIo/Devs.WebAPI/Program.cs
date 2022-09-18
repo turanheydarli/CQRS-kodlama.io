8387fe6d130e2fc1b772a18c9f7eb3d67b53ba25
Turan Heydarli <info@yennimusic.com>
2022-09-19 01:47:52 +0400

    test git

--- src/kodlamaIo/Devs.WebAPI/Program.cs
+++ src/kodlamaIo/Devs.WebAPI/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCoreApplicationServices();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); 
 
 var app = builder.Build();
 
